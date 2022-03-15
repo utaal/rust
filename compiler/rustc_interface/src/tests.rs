10684b904a490ad2065e038cc8d68a025069eae4
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2022-03-15 15:53:23 +0100

    Add seemingly necessary type annotation in compiler/rustc_interface/src/tests.rs

--- compiler/rustc_interface/src/tests.rs
+++ compiler/rustc_interface/src/tests.rs
@@ -726,7 +726,7 @@ fn test_debugging_options_tracking_hash() {
     tracked!(dep_info_omit_d_target, true);
     tracked!(dual_proc_macros, true);
     // formal verifier:
-    tracked!(enable_feature, vec![]);
+    tracked!(enable_feature, Vec::<String>::new());
     tracked!(fewer_names, Some(true));
     tracked!(force_unstable_if_unmarked, true);
     tracked!(fuel, Some(("abc".to_string(), 99)));
